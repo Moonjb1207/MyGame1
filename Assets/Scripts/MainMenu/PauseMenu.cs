9a162b55015f9178d2ef0cc055a56e5c915cf8a4
Moonjb1207 <87357402+Moonjb1207@users.noreply.github.com>
2023-01-11 13:20:40 +0900

    230110_2
    PauseMenu_2

--- Assets/Scripts/MainMenu/PauseMenu.cs
+++ Assets/Scripts/MainMenu/PauseMenu.cs
@@ -4,6 +4,46 @@ using UnityEngine;
 
 public class PauseMenu : MonoBehaviour
 {
+    enum MenuState
+    {
+        Main, Setting, Help,
+    }
+
+    MenuState myState = MenuState.Main;
+    public GameObject[] myMenu;
+
+    void ChangeState(MenuState s)
+    {
+        if (myState == s) return;
+
+        myState = s;
+
+        switch(myState)
+        {
+            case MenuState.Main:
+                foreach (GameObject i in myMenu)
+                {
+                    i.SetActive(false);
+                }
+                myMenu[(int)MenuState.Main].SetActive(true);
+                break;
+            case MenuState.Setting:
+                foreach (GameObject i in myMenu)
+                {
+                    i.SetActive(false);
+                }
+                myMenu[(int)MenuState.Setting].SetActive(true);
+                break;
+            case MenuState.Help:
+                foreach (GameObject i in myMenu)
+                {
+                    i.SetActive(false);
+                }
+                myMenu[(int)MenuState.Help].SetActive(true);
+                break;
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +54,40 @@ public class PauseMenu : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void MainSettingButton()
+    {
+        ChangeState(MenuState.Setting);
+    }
+
+    public void MainHelpButton()
+    {
+        ChangeState(MenuState.Help);
+    }
+
+    public void MainMainSceneButton()
+    {
+
+    }
+
+    public void MainQuitButton()
+    {
+
+    }
+
+    public void SettingBackButton()
+    {
+        ChangeState(MenuState.Main);
+    }
+
+    public void SettingSaveButton()
+    {
+
+    }
+
+    public void HelpBackButton()
+    {
+        ChangeState(MenuState.Main);
     }
 }
