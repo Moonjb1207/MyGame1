266bb24fe2ac50cb1ac3bd2bb11154c98d5a9b68
Moonjb1207 <87357402+Moonjb1207@users.noreply.github.com>
2023-06-20 18:41:22 +0900

    230620
    Main_Main_UI Update

--- Assets/Scripts/MainMenu/MainMenu.cs
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class MainMenu : MonoBehaviour
 {
@@ -13,6 +14,8 @@ public class MainMenu : MonoBehaviour
 
         BGSoundManager.Inst.playBG();
     }
+
+    public TMPro.TMP_Text curText;
 
     enum MenuState
     {
