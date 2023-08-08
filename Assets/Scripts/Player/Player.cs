dd45e0dc516c20fd55d13927756058cf2403fbe7
Moon <postick1207@gmail.com>
2023-08-09 00:48:03 +0900

    230809
    JumpBug_Fix_1

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -126,7 +126,7 @@ public class Player : CharacterMovement, IBattle
             case STATE.Create:
                 break;
             case STATE.Normal:
-                if (Physics.Raycast(transform.position, Vector3.down, 0.1f, myGround) && myAnim.GetBool("IsAir"))
+                if (Physics.Raycast(transform.position, Vector3.down, 0.2f, myGround) && myAnim.GetBool("IsAir"))
                 {
                     myAnim.SetBool("IsAir", false);
                     myAnim.SetTrigger("Landing");
